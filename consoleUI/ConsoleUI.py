5c3f9c26d0c9373122eeb695bdf2f72c7da0dc62
Александра Орлова <aleksandra.orlova@aleksandraorlova-nb.local>
2019-03-10 12:29:37 +0300

    fix ui

--- consoleUI/ConsoleUI.py
+++ consoleUI/ConsoleUI.py
@@ -2,20 +2,21 @@ from tcod import tcod
 
 from controller.map_controller import MapController
 from shared.common import CellType
-from shared.player_map import MoveType
+from shared.player_map import MoveType, StateChange, PlayerMove, PlayerToken
 
 
 class ConsoleUI:
-    def __init__(self):
-        pass
+    def __init__(self, player_token):
+        self.player_token = player_token
 
-    def start(self, map):
-        screen_width = len(map.map_array)
-        screen_height = len(map.map_array[0])
-
+    def start(self):
+        map = MapController().get_player_map(self.player_token)
+        screen_width = len(map.map) + 10
+        screen_height = len(map.map[0]) + 10
+        tcod.console_set_custom_font('arial12x12.png', tcod.FONT_TYPE_GREYSCALE | tcod.FONT_LAYOUT_TCOD)
         console = tcod.console_init_root(screen_width, screen_height, 'roguelike aaa', False)
         tcod.sys_set_fps(5)
-        tcod.console_set_default_foreground(0, tcod.white)
+        tcod.console_set_default_foreground(console, tcod.white)
         self._draw(console)
 
     def _draw_map(self, map_array, console):
@@ -24,17 +25,18 @@ class ConsoleUI:
                 tcod.console_put_char(console, i, j, map_array[i][j], tcod.BKGND_NONE)
 
     def _draw_hero(self, pos_x, pos_y, console):
-        tcod.console_put_char(console, pos_x, pos_y, CellType.HERO, tcod.BKGND_NONE)
+        tcod.console_put_char(console, pos_x, pos_y, CellType.HERO.value, tcod.BKGND_NONE)
 
     def _draw(self, console):
         while not tcod.console_is_window_closed():
-            map_ = MapController.get_player_map()
-            self._draw_map(map_.map_array, console)
-            self._draw_hero(map_.pos_x, map_.pos_y, console)
+            map_ = MapController().get_player_map(self.player_token)
+            self._draw_map(map_.map, console)
+            self._draw_hero(map_.player.token.x, map_.player.token.y, console)
             tcod.console_flush()
             key = tcod.console_check_for_keypress()
             action = self._handle_keys(key)
-            MapController.change_state(action)
+            if action:
+                MapController().change_state(StateChange(PlayerMove(action)), self.player_token)
 
     def _handle_keys(self, key):
         if key.vk == tcod.KEY_UP:
@@ -46,4 +48,6 @@ class ConsoleUI:
         elif key.vk == tcod.KEY_RIGHT:
             return MoveType.RIGHT
 
-
+if __name__ == '__main__':
+    ui = ConsoleUI(PlayerToken("pupa"))
+    ui.start()
