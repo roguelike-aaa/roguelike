a06364468421b10e16c0e7295e020664ce4c8ceb
Ann <annikura@yandex.ru>
2019-03-13 23:36:38 +0300

    Fix constructor bug

--- controller/map_controller.py
+++ controller/map_controller.py
@@ -1,6 +1,6 @@
 from controller.session import Session
 from shared.common import Map, CellType, Coordinate
-from shared.player_map import Player, PlayerMap
+from shared.player_map import Player, PlayerMap, PlayerToken
 
 
 class MapController:
@@ -12,7 +12,7 @@ class MapController:
 
                 """
 
-        self.session = Session([Player(Coordinate(1, 2), "pupa")], Map(3, 5, [
+        self.session = Session([Player(Coordinate(1, 2), PlayerToken("pupa"))], Map(3, 5, [
             [CellType.EMPTY_SPACE, CellType.EMPTY_SPACE, CellType.HORIZONTAL_WALL, CellType.HORIZONTAL_WALL, CellType.EMPTY_SPACE],
             [CellType.PATH, CellType.DOOR, CellType.ROOM_SPACE, CellType.ROOM_SPACE, CellType.HORIZONTAL_WALL],
             [CellType.PATH, CellType.EMPTY_SPACE, CellType.HORIZONTAL_WALL, CellType.HORIZONTAL_WALL, CellType.EMPTY_SPACE]
@@ -20,7 +20,7 @@ class MapController:
 
     def get_player_map(self, player_token):
         return PlayerMap(self.session.dump_players_map(player_token),
-                         Player(player_token, self.session.players[player_token].coordinate))
+                         Player(self.session.players[player_token].coordinate, player_token))
 
     def change_state(self, state_change, player_token):
         self.session.change_player_state(player_token, state_change)
