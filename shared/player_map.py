1699e60d85ea26b2032b27ca2ae5116808ae669d
Ann <annikura@yandex.ru>
2019-03-13 23:48:13 +0300

    Add docs to player map classes

--- shared/player_map.py
+++ shared/player_map.py
@@ -2,11 +2,23 @@ import enum
 
 from shared.common import Coordinate
 
+"""
+    Classes for interaction between UI and Controller
+"""
+
 
 # Player state request
+# UI -> (pulls get map method) -> Controller
+# Controller -> (PlayerMap) -> UI
 
 class PlayerToken:
+    """
+        Token unique for each player.
+    """
     def __init__(self, name: str):
+        """
+        :param name: player username.
+        """
         self.name = name
 
     def __eq__(self, other):
@@ -17,21 +29,38 @@ class PlayerToken:
 
 
 class Player:
+    """
+        Class storing player position on the map.
+    """
     def __init__(self, coordinate: Coordinate, player_token: PlayerToken):
+        """
+        :param coordinate: position on the map.
+        :param player_token: player identifier.
+        """
         self.coordinate = coordinate
         self.token = player_token
 
 
 class PlayerMap:
+    """
+        Class storing a full player knowledge about the game.
+    """
     def __init__(self, player_map, player: Player):
+        """
+        :param player_map: 2-dimensional array of characters describing the map layout visible to player.
+        :param player: player class defining hero state on the map.
+        """
         self.map = player_map
         self.player = player
 
 
 # Change player state requests
-
+# UI -> (pulls the state change method) -> Controller -> Ok!
 
 class MoveType(enum.Enum):
+    """
+        Types of the moves that hero may make.
+    """
     LEFT = enum.auto()
     RIGHT = enum.auto()
     UP = enum.auto()
@@ -39,10 +68,19 @@ class MoveType(enum.Enum):
 
 
 class PlayerMove:
+    """
+        Wrapper for the move type. (In case if some complex moves will appear).
+    """
     def __init__(self, move_type: MoveType):
         self.move_type = move_type
 
 
 class StateChange:
+    """
+        Describes the player's state change.
+    """
     def __init__(self, player_move: PlayerMove):
+        """
+        :param player_move: relative move that player made.
+        """
         self.player_move = player_move
