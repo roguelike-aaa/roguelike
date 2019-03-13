1ae382f908823715796c47fa40b11f94dcfe7c92
Ann <annikura@yandex.ru>
2019-03-14 01:33:05 +0300

    Add main

--- controller/map_controller.py
+++ controller/map_controller.py
@@ -1,8 +1,7 @@
 from controller.session import Session
 from mapgenerator.generator import generate_map
-from shared.common import Map, CellType, Coordinate
 from shared.map_init import MapConfig
-from shared.player_map import Player, PlayerMap, PlayerToken, GameSettings
+from shared.player_map import Player, PlayerMap, GameSettings
 
 
 class MapController:
@@ -10,27 +9,11 @@ class MapController:
     def __init__(self, player_token, game_settings: GameSettings):
         self.start_new_game(player_token, game_settings)
 
-    def __init__(self):
-        """
-            Sample map:
-
-            '  -- '
-            '#*..|'
-            '# -- '
-        """
-        self.session = Session([Player(Coordinate(1, 2), PlayerToken("pupa"))], Map(3, 5, [
-            [CellType.EMPTY_SPACE, CellType.EMPTY_SPACE, CellType.HORIZONTAL_WALL, CellType.HORIZONTAL_WALL,
-             CellType.EMPTY_SPACE],
-            [CellType.PATH, CellType.DOOR, CellType.ROOM_SPACE, CellType.ROOM_SPACE, CellType.HORIZONTAL_WALL],
-            [CellType.PATH, CellType.EMPTY_SPACE, CellType.HORIZONTAL_WALL, CellType.HORIZONTAL_WALL,
-             CellType.EMPTY_SPACE]
-        ]))
-
     def start_new_game(self, player_token, game_settings: GameSettings):
         config = MapConfig(height=game_settings.map_height,
                            width=game_settings.map_width)
         generated_map = generate_map(config)
-        self.session = Session([Player(generated_map.player_init_states.coordinates, player_token)], generated_map.map)
+        self.session = Session([Player(generated_map.player_init_states.coordinate, player_token)], generated_map.map)
 
     def get_player_map(self, player_token):
         return PlayerMap(self.session.dump_players_map(player_token),
---
+++ main.py
@@ -0,0 +1,29 @@
+import argparse
+
+#from consoleUI.ConsoleUI import ConsoleUI
+from controller.map_controller import MapController
+from shared.player_map import PlayerToken, GameSettings
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Roguelike application")
+    parser.add_argument("--width", "-y", help="Map width", required=True, type=int)
+    parser.add_argument("--height", "-x", help="Map height", required=True, type=int)
+    parser.add_argument("--token", "-t", help="Player username", required=True)
+
+    args = parser.parse_args()
+    controller = MapController(PlayerToken(args.token), GameSettings(args.height, args.width))
+    # ui = ConsoleUI(PlayerToken(args.token))
+
+
+
+#    for line in controller.get_player_map(PlayerToken(args.token)).map:
+#        for c in line:
+#            print(c, end='')
+#        print()
+
+    # ui.start(controller)
+
+
+if __name__ == "__main__":
+    main()
