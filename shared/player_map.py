247f853fd9c42b9f1d580ffbe6bbc454680a6034
Ann <annikura@yandex.ru>
2019-03-13 23:36:12 +0300

    Add types to classes

--- shared/player_map.py
+++ shared/player_map.py
@@ -5,20 +5,8 @@ from shared.common import Coordinate
 
 # Player state request
 
-class PlayerMap:
-    def __init__(self, player_map, player):
-        self.map = player_map
-        self.player = player
-
-
-class Player:
-    def __init__(self, coordinate, player_token):
-        self.coordinate = coordinate
-        self.token = player_token
-
-
 class PlayerToken:
-    def __init__(self, name):
+    def __init__(self, name: str):
         self.name = name
 
     def __eq__(self, other):
@@ -28,16 +16,19 @@ class PlayerToken:
         return self.name.__hash__()
 
 
+class Player:
+    def __init__(self, coordinate: Coordinate, player_token: PlayerToken):
+        self.coordinate = coordinate
+        self.token = player_token
+
+
+class PlayerMap:
+    def __init__(self, player_map, player: Player):
+        self.map = player_map
+        self.player = player
+
+
 # Change player state requests
-
-class StateChange:
-    def __init__(self, player_move):
-        self.player_move = player_move
-
-
-class PlayerMove:
-    def __init__(self, move_type):
-        self.move_type = move_type
 
 
 class MoveType(enum.Enum):
@@ -45,3 +36,13 @@ class MoveType(enum.Enum):
     RIGHT = enum.auto()
     UP = enum.auto()
     DOWN = enum.auto()
+
+
+class PlayerMove:
+    def __init__(self, move_type: MoveType):
+        self.move_type = move_type
+
+
+class StateChange:
+    def __init__(self, player_move: PlayerMove):
+        self.player_move = player_move
