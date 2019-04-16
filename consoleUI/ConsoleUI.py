dd7fd3bb91d45673568ab13e9851e421fea5c559
Александра Орлова <aleksandra.orlova@192-168-0-101.local>
2019-04-17 00:57:25 +0300

    add new ui part

--- consoleUI/ConsoleUI.py
+++ consoleUI/ConsoleUI.py
@@ -12,7 +12,7 @@ class ConsoleUI:
     def start(self):
         map = self.__controller.get_player_map(self.__player_token)
         screen_width = len(map.map) + 10
-        screen_height = len(map.map[0]) + 10
+        screen_height = len(map.map[0]) + 20
         tcod.console_set_custom_font('arial12x12.png', tcod.FONT_TYPE_GREYSCALE | tcod.FONT_LAYOUT_TCOD)
         console = tcod.console_init_root(screen_width, screen_height, 'Roguelike AAA', False)
         tcod.sys_set_fps(5)
@@ -27,11 +27,33 @@ class ConsoleUI:
     def _draw_hero(self, pos_x, pos_y, console):
         tcod.console_put_char(console, pos_y, pos_x, CellType.HERO.value, tcod.BKGND_NONE)
 
+    def _write_status(self, console, map):
+        status = "STATUS:"
+        padding = 0
+        for s in status:
+            tcod.console_put_char(console, padding, len(map) + 10, s, tcod.BKGND_NONE)
+            padding += 1
+        for s in map.status_message:
+            tcod.console_put_char(console, padding, len(map) + 10, s, tcod.BKGND_NONE)
+            padding += 1
+
+    def _write_health(self, console, map):
+        status = "HEALTH:"
+        padding = 0
+        for s in status:
+            tcod.console_put_char(console, padding, len(map) + 15, s, tcod.BKGND_NONE)
+            padding += 1
+        for s in str(map.player.player_fight_stats):
+            tcod.console_put_char(console, padding, len(map) + 15, s, tcod.BKGND_NONE)
+            padding += 1
+
     def _draw(self, console):
         while not tcod.console_is_window_closed():
             map_ = self.__controller.get_player_map(self.__player_token)
             self._draw_map(map_.map, console)
             self._draw_hero(map_.player.coordinate.x, map_.player.coordinate.y, console)
+            self._write_status(console, map_)
+            self._write_health(console, map_)
             tcod.console_flush()
             key = tcod.console_check_for_keypress()
             action = self._handle_keys(key)
