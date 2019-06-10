bee1072a2c8e3b83dc63f1a0b3ba4d578f997ee4
Александра Орлова <aleksandra.orlova@aleksandraorlova-nb.local>
2019-06-11 00:39:48 +0300

    add inventory

--- consoleUI/ConsoleUI.py
+++ consoleUI/ConsoleUI.py
@@ -13,13 +13,18 @@ class ConsoleUI:
         self.__map = None
         self.__items = []
         self.__selected_item = 0
+        self.__inventory = None
+        self.__selected_helmet = False
+        self.__selected_shirt = False
+        self.__selected_weapon = False
+
 
     def start(self):
         """
             Start UI
         """
         self.__map = self.__get_map_from_controller().map
-        screen_width = len(self.__map.map) + 40
+        screen_width = len(self.__map.map) + 50
         screen_height = len(self.__map.map[0]) + 20
         tcod.console_set_custom_font('arial12x12.png', tcod.FONT_TYPE_GREYSCALE | tcod.FONT_LAYOUT_TCOD)
         console = tcod.console_init_root(screen_width, screen_height, 'Roguelike AAA', False)
@@ -52,12 +57,14 @@ class ConsoleUI:
                 self.__map = command.map
             elif command.__class__ == SendItemsList:
                 self.__items = list(command.items.items.values())
+                self.__inventory = command.items
 
     def __lifecicle(self, console):
         while not tcod.console_is_window_closed():
             if not self.__commandReceiver.is_empty():
                 self.__make_command()
             self.__write_items(console, self.__items, self.__map)
+            self.__write_inventory(console)
             self.__draw_map(self.__map.map, console)
             self.__draw_hero(self.__map.player.coordinate.x, self.__map.player.coordinate.y, console)
             self.__write_status(console, self.__map)
@@ -82,16 +89,62 @@ class ConsoleUI:
         elif key.vk in range(tcod.KEY_1, tcod.KEY_9 + 1):
             self.__selected_item = key.vk - tcod.KEY_1 + 1
         elif key.vk == tcod.KEY_CHAR:
-            if key.c == ord('q') and 0 < self.__selected_item <= len(self.__items):
-                self.__send_item_action(ItemActionType.DROP, self.__items[self.__selected_item - 1])
-            if key.c == ord('w') and 0 < self.__selected_item <= len(self.__items):
-                self.__send_item_action(ItemActionType.REMOVE, self.__items[self.__selected_item - 1])
-            if key.c == ord('e') and 0 < self.__selected_item <= len(self.__items):
-                self.__send_item_action(ItemActionType.USE, self.__items[self.__selected_item - 1])
+            if key.c == ord('q') and 0 < self.__selected_item <= len(self.__items) \
+                    or self.__selected_helmet or self.__selected_shirt or self.__selected_weapon:
+                if key.c == ord('q') and 0 < self.__selected_item <= len(self.__items):
+                    self.__send_item_action(ItemActionType.DROP, self.__items[self.__selected_item - 1])
+                elif self.__selected_helmet:
+                    self.__send_item_action(ItemActionType.DROP, self.__inventory.active_helmet)
+                elif self.__selected_shirt:
+                    self.__send_item_action(ItemActionType.DROP, self.__inventory.active_shirt)
+                elif self.__selected_weapon:
+                    self.__send_item_action(ItemActionType.DROP, self.__inventory.active_weapon)
+            if key.c == ord('w') and 0 < self.__selected_item <= len(self.__items) \
+                    or self.__selected_helmet or self.__selected_shirt or self.__selected_weapon:
+                if key.c == ord('w') and 0 < self.__selected_item <= len(self.__items):
+                    self.__send_item_action(ItemActionType.USE, self.__items[self.__selected_item - 1])
+                elif self.__selected_helmet:
+                    self.__send_item_action(ItemActionType.USE, self.__inventory.active_helmet)
+                elif self.__selected_shirt:
+                    self.__send_item_action(ItemActionType.USE, self.__inventory.active_shirt)
+                elif self.__selected_weapon:
+                    self.__send_item_action(ItemActionType.USE, self.__inventory.active_weapon)
+            if key.c == ord('e') and 0 < self.__selected_item <= len(self.__items) \
+                    or self.__selected_helmet or self.__selected_shirt or self.__selected_weapon:
+                if key.c == ord('e') and 0 < self.__selected_item <= len(self.__items):
+                    self.__send_item_action(ItemActionType.WEAR, self.__items[self.__selected_item - 1])
+                elif self.__selected_helmet:
+                    self.__send_item_action(ItemActionType.WEAR, self.__inventory.active_helmet)
+                elif self.__selected_shirt:
+                    self.__send_item_action(ItemActionType.WEAR, self.__inventory.active_shirt)
+                elif self.__selected_weapon:
+                    self.__send_item_action(ItemActionType.WEAR, self.__inventory.active_weapon)
+            if key.c == ord('r') and 0 < self.__selected_item <= len(self.__items) \
+                    or self.__selected_helmet or self.__selected_shirt or self.__selected_weapon:
+                if key.c == ord('r') and 0 < self.__selected_item <= len(self.__items):
+                    self.__send_item_action(ItemActionType.REMOVE_FROM_SLOT, self.__items[self.__selected_item - 1])
+                elif self.__selected_helmet:
+                    self.__send_item_action(ItemActionType.REMOVE_FROM_SLOT, self.__inventory.active_helmet)
+                elif self.__selected_shirt:
+                    self.__send_item_action(ItemActionType.REMOVE_FROM_SLOT, self.__inventory.active_shirt)
+                elif self.__selected_weapon:
+                    self.__send_item_action(ItemActionType.REMOVE_FROM_SLOT, self.__inventory.active_weapon)
             if key.c == ord('s'):
                 self.__commandSender.put(SaveGame())
             if key.c == ord('l'):
                 self.__commandSender.put(LoadGame())
+            if key.c == ord('i') and self.__inventory.active_helmet:
+                self.__selected_helmet = True
+                self.__selected_shirt = False
+                self.__selected_weapon = False
+            if key.c == ord('o') and self.__inventory.active_shirt:
+                self.__selected_helmet = False
+                self.__selected_shirt = True
+                self.__selected_weapon = False
+            if key.c == ord('p') and self.__inventory.active_weapon:
+                self.__selected_helmet = False
+                self.__selected_shirt = False
+                self.__selected_weapon = True
             return None
         else:
             return None
@@ -121,11 +174,13 @@ class ConsoleUI:
     def __write_health(console, map):
         status = "HEALTH:"
         padding = 0
-        for s in status:
+        for s in status + str(map.player.fight_stats.get_health()):
             tcod.console_put_char(console, padding, len(map.map) + 12, s, tcod.BKGND_NONE)
             padding += 1
-        for s in str(map.player.fight_stats.get_health()):
-            tcod.console_put_char(console, padding, len(map.map) + 12, s, tcod.BKGND_NONE)
+        status = "STRENGTH:"
+        padding = 0
+        for s in status + str(map.player.fight_stats.get_strength()):
+            tcod.console_put_char(console, padding, len(map.map) + 14, s, tcod.BKGND_NONE)
             padding += 1
 
     def __write_items(self, console, items, map):
@@ -138,6 +193,17 @@ class ConsoleUI:
             else:
                 self.__write_item(console, item, padding_right, padding_top)
             padding_top += 1
+
+    def __write_inventory(self, console):
+        if not self.__inventory:
+            return
+        padding_top = len(self.__map.map) + 16
+        if self.__inventory.active_helmet:
+            self.__write_item(console, self.__inventory.active_helmet, 0, padding_top)
+        if self.__inventory.active_shirt:
+            self.__write_item(console, self.__inventory.active_shirt, 0, padding_top)
+        if self.__inventory.active_weapon:
+            self.__write_item(console, self.__inventory.active_weapon, 0, padding_top)
 
     @staticmethod
     def __write_item(console, item, padding_right, padding_top, selected=False):
